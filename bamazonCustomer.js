3f5f1d74264c8bd025ef0dfef1ef8007ac12612e
fitzpaky <kfitzpatrick44@gmail.com>
2020-04-13 09:13:45 -0700

    adding console message

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -26,25 +26,12 @@ function productsForSale() {
     connection.query(query, function (err, res) {
         if (err) throw err;
         for (var i = 0; i < res.length; i++) {
-            console.log("Product: " + res[i].product_name + " || Department: " + res[i].department_name + " || Price: " + res[i].price + "|| Quantity: " + res[i].stock_quantity);
+            console.log("Id: " + res[i].item_id + " || Product: " + res[i].product_name + " || Department: " + res[i].department_name + " || Price: " + res[i].price + "|| Quantity: " + res[i].stock_quantity);
         }
         runSearch();
     })
 }
 
-// Then create a Node application called bamazonCustomer.js. Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
-
-// The app should then prompt users with two messages.
-
-// The first should ask them the ID of the product they would like to buy.
-// The second message should ask how many units of the product they would like to buy.
-// Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
-
-// If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
-// However, if your store does have enough of the product, you should fulfill the customer's order.
-
-// This means updating the SQL database to reflect the remaining quantity.
-// Once the update goes through, show the customer the total cost of their purchase.
 function runSearch() {
     inquirer
         .prompt({
@@ -97,7 +84,8 @@ function idSearch() {
                         console.log(`\nSorry, there is ${res[0].stock_quantity} left!\n`)
                     }
                     else {
-                        console.log(`You chose ${answer.units} ${res[0].product_name}. Your total is: $ ${answer.units * res[0].price}`);
+                        console.log(`\x1b[32m%s\x1b[0m`, `\nSufficient quantity!`)
+                        console.log(`\nYou chose ${answer.units} ${res[0].product_name}. Your total is: $ ${answer.units * res[0].price}\n`);
                         var quantityLeft = res[0].stock_quantity - answer.units;
                         console.log(quantityLeft);
                         connection.query(
