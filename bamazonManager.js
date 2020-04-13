c305c6a974a8972d0d8a6786b9cd14b7dc358f97
fitzpaky <kfitzpatrick44@gmail.com>
2020-04-13 09:31:34 -0700

    adding choices

---
+++ bamazonManager.js
@@ -0,0 +1,177 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "Lucy!123",
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    productsForSale();
+
+});
+
+
+// If a manager selects Add to Inventory, your app should display a prompt that will let the manager "add more" of any item currently in the store.
+
+// If a manager selects Add New Product, it should allow the manager to add a completely new product to the store.
+function runSearch() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View Products for Sale",
+          "View Low Inventory",
+          "Add to Inventory",
+          "Add New Product",
+          "exit"
+        ]
+      })
+      .then(function(answer) {
+        switch (answer.action) {
+        case "View Products for Sale":
+          productsForSale();
+          break;
+  
+        case "View Low Inventory":
+          lowInventory();
+          break;
+          
+        case "Add to Inventory":
+          addInventory();
+          break;
+  
+        case "Add New Product":
+          addNewProduct();
+          break;
+  
+        case "exit":
+          connection.end();
+          break;
+        }
+      });
+  }
+  // If a manager selects View Low Inventory, then it should list all items with an inventory count lower than five.
+
+  function lowInventory() {
+    inquirer
+      .prompt({
+        name: "artist",
+        type: "input",
+        message: "What artist would you like to search for?"
+      })
+      .then(function(answer) {
+        var query = "SELECT position, song, year FROM top5000 WHERE ?";
+        connection.query(query, { artist: answer.artist }, function(err, res) {
+          if (err) throw err;
+          for (var i = 0; i < res.length; i++) {
+            console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
+          }
+          runSearch();
+        });
+      });
+  }
+  
+  // If a manager selects View Products for Sale, the app should list every available item: the item IDs, names, prices, and quantities.
+
+function productsForSale() {
+    var query = "SELECT * FROM products";
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            console.log("Id: " + res[i].item_id + " || Product: " + res[i].product_name + " || Department: " + res[i].department_name + " || Price: " + res[i].price + "|| Quantity: " + res[i].stock_quantity);
+        }
+        runSearch();
+    })
+}
+
+function runSearch() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "Find products by id",
+                "exit"
+            ]
+        })
+        .then(function (answer) {
+            switch (answer.action) {
+                case "Find products by id":
+                    idSearch();
+                    break;
+
+                case "Find units of product":
+                    unitsSearch();
+                    break;
+
+                case "exit":
+                    connection.end();
+                    break;
+            }
+        });
+}
+
+function idSearch() {
+    inquirer
+        .prompt([
+            {
+                name: "id",
+                type: "input",
+                message: "What is the id of the product you would like to buy?"
+            },
+            {
+                name: "units",
+                type: "input",
+                message: "How many units of the product you would like to buy?"
+            }
+        ])
+        .then(function(answer) {
+            connection.query("SELECT item_id, product_name, stock_quantity, price FROM products WHERE ?",
+                {item_id: answer.id}, function (err, res) {
+                    if (err) throw err;
+
+                    if (answer.units > res[0].stock_quantity) {
+                        console.log(`\x1b[31m%s\x1b[0m`, `\nInsufficient quantity!`)
+                        console.log(`\nSorry, there is ${res[0].stock_quantity} left!\n`)
+                    }
+                    else {
+                        console.log(`\x1b[32m%s\x1b[0m`, `\nSufficient quantity!`)
+                        console.log(`\nYou chose ${answer.units} ${res[0].product_name}. Your total is: $ ${answer.units * res[0].price}\n`);
+                        var quantityLeft = res[0].stock_quantity - answer.units;
+                        console.log(quantityLeft);
+                        connection.query(
+                            "UPDATE products SET ? WHERE ?",
+                            [
+                                {
+                                    stock_quantity: quantityLeft
+                                },
+                                {
+                                    item_id: answer.id
+                                }
+                            ],
+                            function (error) {
+                                if (error) throw err;
+
+                            });
+                    }
+                    runSearch();
+
+                }
+            );
+        });
+}
+
